aa2e5dcd5f375196a8520a3853784cd62661073c
UKG๛LiZarD <106474203+Buleguppy@users.noreply.github.com>
2025-11-28 18:53:23 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1448,13 +1448,19 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
     elif query.data == "start":
         buttons = [[
-                    InlineKeyboardButton('🔰 ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ 🔰', url=f'http://telegram.me/{temp.U_NAME}?startgroup=true')
-                ],[
-                    InlineKeyboardButton(' ʜᴇʟᴘ 📢', callback_data='help'),
-                    InlineKeyboardButton(' ᴀʙᴏᴜᴛ 📖', callback_data='about')
-                ],[
-                    InlineKeyboardButton('ᴛᴏᴘ sᴇᴀʀᴄʜɪɴɢ ⭐', callback_data="topsearch"),
-                     InlineKeyboardButton('ᴜᴘɢʀᴀᴅᴇ 🎟', callback_data="premium_info"),
+                    InlineKeyboardButton('× 𝑨𝑫𝑫 𝑴𝑬 𝑻𝑶 𝒀𝑶𝑼𝑹 𝑮𝑹𝑶𝑼𝑷 ×', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            ],[
+            InlineKeyboardButton('𝑴𝑶𝑽𝑰𝑬 𝑮𝑹𝑶𝑼𝑷 🎬', url='https://t.me/planet_movies_grp'),
+            InlineKeyboardButton('𝑼𝑷𝑫𝑨𝑻𝑬𝑺 📢', url='https://t.me/planet_movies_links')
+            ],[
+            InlineKeyboardButton('𝑪𝑶𝑵𝑻𝑨𝑪𝑻 𝑫𝑬𝑽 💬', url='https://t.me/LiZarD_amc')
+            ],[      
+            InlineKeyboardButton('𝑯𝑬𝑳𝑷 ℹ️', callback_data='help'),
+            InlineKeyboardButton('𝑨𝑩𝑶𝑼𝑻 🤠', callback_data='about')
+            ],[
+            InlineKeyboardButton('🔍 𝑺𝑬𝑨𝑹𝑪𝑯 𝑯𝑬𝑹𝑬 𝑴𝑶𝑽𝑰𝑬𝑺 🔎', switch_inline_query_current_chat=''),
+            ],[
+            InlineKeyboardButton('𝑪𝑹𝑬𝑫𝑰𝑻 💫', url='https://t.me/LiZarD_amc'), 
                 ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         current_time = datetime.now(pytz.timezone(TIMEZONE))
